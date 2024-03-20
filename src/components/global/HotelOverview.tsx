15b7c438124e8780cdc1e0fbc8494342e95f346b
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-03-20 22:12:23 +0700

    HotelOverview Reusable Components

---
+++ src/components/global/HotelOverview.tsx
@@ -0,0 +1,70 @@
+import { Box, Grid, IconButton, Typography } from "@mui/material";
+import ContentWrapper from "./ContentWrapper";
+import HeaderBottom from "./HeaderBottom";
+import Image from "next/image";
+
+import IconArrowLeft from "@/assets/icons/general/btn-icon-arrow-left.svg";
+
+const HotelOverview = (props: {
+  header: string[];
+  content: string[];
+  image: any;
+  illus?: any;
+}) => {
+  return (
+    <ContentWrapper>
+      <HeaderBottom
+        leftComponent={<Typography>{props.header[0]}</Typography>}
+        middleComponent={<Typography>{props.header[1]}</Typography>}
+        rightComponent={<Typography>{props.header[2]}</Typography>}
+      />
+      <Grid
+        container
+        direction={"row-reverse"}
+        alignItems={"center"}
+        marginTop={5}
+      >
+        <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
+          <Image
+            src={props.image}
+            alt={"landside  -image"}
+            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            alignItems={"start"}
+            width={"100%"}
+            paddingRight={5}
+          >
+            <Typography variant="body2">{props.content[0]}</Typography>
+            <Typography width={"70%"}>{props.content[1]}</Typography>
+            <Box
+              display={"flex"}
+              width={"100%"}
+              flexDirection={"row"}
+              justifyContent={"space-between"}
+              alignItems={"start"}
+              marginTop={4}
+            >
+              <IconButton sx={{ transform: "scaleX(-1)" }}>
+                <Image
+                  src={IconArrowLeft}
+                  alt="arrow-left"
+                  style={{ transform: "scaleX(-1)" }}
+                />
+              </IconButton>
+              {props.illus && (
+                <Image src={props.illus} alt="capsult-transit-newly-launched" />
+              )}
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ContentWrapper>
+  );
+};
+
+export default HotelOverview;
--- src/components/klia-2/AirsideOverviewKLIA2.tsx
+++ src/components/klia-2/AirsideOverviewKLIA2.tsx
@@ -1,10 +1,6 @@
-import { Box, Grid, IconButton, Typography } from "@mui/material";
-import HeaderBottom from "../global/HeaderBottom";
-import ContentWrapper from "../global/ContentWrapper";
-import Image from "next/image";
-
 import AirsideImage from "./images/klia2-airside@2x.jpg";
 import IconArrowLeft from "@/assets/icons/general/btn-icon-arrow-left.svg";
+import HotelOverview from "../global/HotelOverview";
 
 const headerContent = ["01", "/03", "Restricted Area"];
 
@@ -16,54 +12,11 @@ const textContent = [
 
 const AirsideOverviewKLIA2 = () => {
   return (
-    <ContentWrapper>
-      <HeaderBottom
-        leftComponent={<Typography>{headerContent[0]}</Typography>}
-        middleComponent={<Typography>{headerContent[1]}</Typography>}
-        rightComponent={<Typography>{headerContent[2]}</Typography>}
-      />
-      <Grid
-        container
-        direction={"row-reverse"}
-        alignItems={"center"}
-        marginTop={5}
-      >
-        <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
-          <Image
-            src={AirsideImage}
-            alt={"airside-image"}
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-          <Box
-            display={"flex"}
-            flexDirection={"column"}
-            alignItems={"start"}
-            width={"100%"}
-            paddingRight={5}
-          >
-            <Typography variant="body2">{textContent[0]}</Typography>
-            <Typography width={"70%"}>{textContent[1]}</Typography>
-            <Box
-              display={"flex"}
-              flexDirection={"row"}
-              justifyContent={"space-between"}
-              alignItems={"start"}
-              marginTop={4}
-            >
-              <IconButton sx={{ transform: "scaleX(-1)" }}>
-                <Image
-                  src={IconArrowLeft}
-                  alt="arrow-left"
-                  style={{ transform: "scaleX(-1)" }}
-                />
-              </IconButton>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ContentWrapper>
+    <HotelOverview
+      header={headerContent}
+      content={textContent}
+      image={AirsideImage}
+    />
   );
 };
 
