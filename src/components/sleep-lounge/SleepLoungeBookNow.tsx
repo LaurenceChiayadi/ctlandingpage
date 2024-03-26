3e46f9af49aad8cbcdfe8dca0cee70498c959ffe
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-03-26 11:01:35 +0700

    Fix Typo

--- src/components/sleep-lounge/SleepLoungeBookNow.tsx
+++ src/components/sleep-lounge/SleepLoungeBookNow.tsx
@@ -1,7 +1,7 @@
 import HeroImage from "./images/hero-sleep-lounge.jpg";
 import HotelBookNow from "../global/hotel/HotelBookNow";
 
-const title = "CapsuleTransit Airside";
+const title = "CapsuleTransit Sleep Lounge";
 
 const SleepLoungeBookNow = () => {
   return <HotelBookNow HeroImage={HeroImage} title={title} />;
