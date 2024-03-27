8ad0d07ec8a73ee9b7846b5047ede1392c7df5e6
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-03-28 00:04:08 +0700

    CT Dedication Mobile View

--- src/components/Homepage/CTDedication.tsx
+++ src/components/Homepage/CTDedication.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, useMediaQuery } from "@mui/material";
 import ContentWrapper from "../global/ContentWrapper";
 import CTButton from "../global/CTButton";
 import Image from "next/image";
@@ -11,6 +11,7 @@ import EdgeTVIcon from "./images/featured_on/logo-featured-theedgetv-bw@2x.png";
 import ETBIcon from "./images/featured_on/logo-featured-etb-bw@2x.png";
 import TV3Icon from "./images/featured_on/logo-featured-tv3-bw@2x.png";
 import BernamaIcon from "./images/featured_on/logo-featured-bernama-bw@2x.png";
+import { motion } from "framer-motion";
 
 const contentTexts = [
   {
@@ -31,14 +32,27 @@ const staticText = ["Featured On"];
 
 const CTDedication = () => {
   const handleCommitmentClick = () => {};
+  const isHandheldDevice = useMediaQuery("(max-width:1050px)");
   return (
     <ContentWrapper>
+      {!isHandheldDevice ? (
+        <DesktopView handleCommitmentClick={handleCommitmentClick} />
+      ) : (
+        <HandheldView handleCommitmentClick={handleCommitmentClick} />
+      )}
+    </ContentWrapper>
+  );
+};
+
+const DesktopView = (props: { handleCommitmentClick: VoidFunction }) => {
+  return (
+    <>
       <Box display={"flex"} flexDirection={"column"} marginY={4} width={"50%"}>
         <Typography variant="h3">{contentTexts[0].text}</Typography>
       </Box>
       <Box marginTop={10} marginBottom={"200px"}>
         <CTButton
-          onClick={handleCommitmentClick}
+          onClick={props.handleCommitmentClick}
           text="Our Commitment"
           variant="secondary"
         />
@@ -76,7 +90,130 @@ const CTDedication = () => {
           </Grid>
         ))}
       </Grid>
-    </ContentWrapper>
+    </>
+  );
+};
+
+const HandheldView = (props: { handleCommitmentClick: VoidFunction }) => {
+  return (
+    <>
+      <Box display={"flex"} flexDirection={"column"} marginY={4} width={"100%"}>
+        <Typography variant="h3">{contentTexts[0].text}</Typography>
+      </Box>
+      <Box marginTop={10} marginBottom={"100px"}>
+        <CTButton
+          onClick={props.handleCommitmentClick}
+          text="Our Commitment"
+          variant="secondary"
+        />
+      </Box>
+      <Box display={"flex"} alignItems={"end"} width={"100%"}>
+        <Image
+          src={IllustrationRestingLady}
+          alt="cloud-man-walkin"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </Box>
+      <Grid
+        container
+        borderTop={1}
+        borderBottom={1}
+        paddingY={5}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+          <Box display={"flex"} justifyContent={"center"} marginBottom={4}>
+            <Typography>{staticText[0]}</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={12} md={1.5} lg={1.5} xl={1.5}>
+          <div
+            style={{ position: "relative", width: "100%", overflow: "hidden" }}
+          >
+            <motion.div
+              animate={{
+                x: ["-200%", "0%"],
+                transition: {
+                  ease: "linear",
+                  duration: 10,
+                  repeat: Infinity,
+                },
+              }}
+              style={{ display: "flex", flexDirection: "row" }}
+            >
+              <Box
+                display={"flex"}
+                flexShrink={0}
+                width={`${100 / featuredOn.length}%`}
+                marginX={4}
+              >
+                <Image
+                  src={featuredOn[featuredOn.length - 2].src}
+                  alt="featured-on"
+                  width={80}
+                  height={50}
+                  style={{
+                    marginRight: "10px", // Adjust spacing between icons as needed
+                  }}
+                />
+              </Box>
+              <Box
+                display={"flex"}
+                flexShrink={0}
+                width={`${100 / featuredOn.length}%`}
+                marginX={4}
+              >
+                <Image
+                  src={featuredOn[featuredOn.length - 1].src}
+                  alt="featured-on"
+                  width={80}
+                  height={50}
+                  style={{
+                    marginRight: "10px", // Adjust spacing between icons as needed
+                  }}
+                />
+              </Box>
+              {featuredOn.map((icon, index) => (
+                <Box
+                  key={index}
+                  display={"flex"}
+                  flexShrink={0}
+                  width={`${100 / featuredOn.length}%`}
+                  marginX={4}
+                >
+                  <Image
+                    src={icon.src}
+                    alt="featured-on"
+                    width={80}
+                    height={50}
+                    style={{
+                      marginRight: "10px", // Adjust spacing between icons as needed
+                    }}
+                  />
+                </Box>
+              ))}
+              <Box
+                display={"flex"}
+                flexShrink={0}
+                width={`${100 / featuredOn.length}%`}
+                marginX={4}
+              >
+                <Image
+                  src={featuredOn[0].src}
+                  alt="featured-on"
+                  width={80}
+                  height={50}
+                  style={{
+                    marginRight: "10px", // Adjust spacing between icons as needed
+                  }}
+                />
+              </Box>
+            </motion.div>
+          </div>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
