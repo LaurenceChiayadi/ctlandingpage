1b47a3020681e793cdb61730c1a3229cf5ba3793
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-03-27 14:44:39 +0700

    Homepage Hero and Intro Page Mobile Responsive

--- src/components/Homepage/HeroSection.tsx
+++ src/components/Homepage/HeroSection.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Box, Stack, Typography, useTheme } from "@mui/material";
+import { Box, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import HeroBackground from "./images/hero-home-img@2x.jpg";
 
 const textContents = [
@@ -9,6 +9,14 @@ const textContents = [
 ];
 
 const HeroSection = () => {
+  const theme = useTheme();
+
+  const isHandheldDevice = useMediaQuery("(max-width:1050px)");
+
+  return !isHandheldDevice ? <DesktopView /> : <HandheldView />;
+};
+
+const DesktopView = () => {
   const theme = useTheme();
   return (
     <Box
@@ -31,6 +39,55 @@ const HeroSection = () => {
         sx={{
           position: "absolute",
           paddingX: 7,
+          paddingBottom: 2,
+        }}
+      >
+        <Stack direction={"row"} spacing={1}>
+          <Typography variant={"h6"} color={theme.palette.CtColorScheme.white}>
+            {textContents[0].text}
+          </Typography>
+          <Typography variant="h6" color={theme.palette.primary.main}>
+            /
+          </Typography>
+          <Typography variant={"h6"} color={theme.palette.CtColorScheme.white}>
+            {textContents[1].text}
+          </Typography>
+        </Stack>
+        <Typography
+          variant="h1"
+          fontWeight={500}
+          color={theme.palette.CtColorScheme.white}
+        >
+          {textContents[2].text}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+const HandheldView = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      marginBottom={"100px"}
+      width={"100%"}
+      sx={{ position: "relative", height: "83vh" }}
+    >
+      <Image
+        src={HeroBackground}
+        alt="CapsuleHero"
+        layout="fill"
+        objectPosition="center"
+      />
+      <Box
+        display={"flex"}
+        height={"100%"}
+        flexDirection={"column"}
+        justifyContent={"end"}
+        width={"370px"}
+        sx={{
+          position: "absolute",
+          paddingX: 3,
           paddingBottom: 2,
         }}
       >
