ffbfec91c898362adbc0f0b81ce17aaf87c3910d
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-03-28 10:58:53 +0700

    Newsroom Mobile Responsive

--- src/components/news/NewsRoomHero.tsx
+++ src/components/news/NewsRoomHero.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, useTheme } from "@mui/material";
+import { Box, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Image from "next/image";
 
 import NewsRoomHeroImage from "./images/hero-bg-newswroom.jpg";
@@ -6,6 +6,12 @@ import NewsRoomHeroImage from "./images/hero-bg-newswroom.jpg";
 const textContent = ["Newsroom", "Stay", "Updated"];
 
 const NewsRoomHero = () => {
+  const theme = useTheme();
+  const isHandheldDevice = useMediaQuery("(max-width:1050px)");
+  return !isHandheldDevice ? <DesktopView /> : <HandheldView />;
+};
+
+const DesktopView = () => {
   const theme = useTheme();
   return (
     <Box
@@ -26,6 +32,54 @@ const NewsRoomHero = () => {
           position: "absolute",
           zIndex: -1,
           width: "100%",
+          height: "100%",
+        }}
+      />
+      <Stack spacing={2} justifyContent={"center"} alignItems={"center"}>
+        <Typography variant={"h2"} color={theme.palette.CtColorScheme.white}>
+          {textContent[0]}
+        </Typography>
+        <div
+          style={{
+            display: "inline-block",
+          }}
+        >
+          <Typography
+            variant="h6"
+            display="inline"
+            bgcolor={theme.palette.primary.main}
+            padding="5px"
+          >
+            {textContent[1]} <b>{textContent[2]}</b>
+          </Typography>
+        </div>
+      </Stack>
+    </Box>
+  );
+};
+
+const HandheldView = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      display={"flex"}
+      width="100%"
+      justifyContent={"center"}
+      alignItems={"center"}
+      sx={{
+        position: "relative",
+        height: "50vh",
+        overflow: "hidden",
+      }}
+    >
+      <Image
+        src={NewsRoomHeroImage}
+        alt="NewsRoomHero"
+        style={{
+          position: "absolute",
+          zIndex: -1,
+          // maxWidth: "100%",
+          // maxHeight: "100%",
           height: "100%",
         }}
       />
