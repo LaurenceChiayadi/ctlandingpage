cc2d4f1fc6eee651908d72b5b0ca2311330fd367
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-03-28 10:51:00 +0700

    Highlight Mobile Responsive

--- src/components/highlight/HighlightHero.tsx
+++ src/components/highlight/HighlightHero.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import Image from "next/image";
 
 import IllustrationHighlight from "./images/illus-highlight.svg";
@@ -11,7 +11,13 @@ const textContent = [
 
 const HighlightHero = () => {
   const theme = useTheme();
+  const isHandheldDevice = useMediaQuery("(max-width:1050px)");
 
+  return !isHandheldDevice ? <DesktopView /> : <HandheldView />;
+};
+
+const DesktopView = () => {
+  const theme = useTheme();
   return (
     <>
       <Box
@@ -51,6 +57,49 @@ const HighlightHero = () => {
         />
       </Box>
     </>
+  );
+};
+
+const HandheldView = () => {
+  const theme = useTheme();
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      height={"100%"}
+      paddingX={3}
+      paddingTop={"200px"}
+    >
+      <div
+        style={{
+          display: "inline-block",
+        }}
+      >
+        <Typography
+          variant="h6"
+          display="inline"
+          bgcolor={theme.palette.primary.main}
+          padding="5px"
+        >
+          {textContent[0]} <b>{textContent[1]}</b>
+        </Typography>
+      </div>
+      <Typography variant="h1" maxWidth={"1000px"}>
+        {textContent[2]}
+      </Typography>
+      <Box display={"flex"} marginTop={5}>
+        <Image
+          src={IllustrationHighlight}
+          alt="cloud-man-walkin"
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            width: "auto",
+            height: "auto",
+          }}
+        />
+      </Box>
+    </Box>
   );
 };
 
