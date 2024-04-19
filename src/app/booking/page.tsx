733152fd387e37b5a892eedfe6c83d30d72f71b8
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-04-19 10:14:50 +0700

    Booking Header and Stepper Mobile Responsive

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -3,22 +3,24 @@
 import { useEffect, useState } from "react";
 import {
   Box,
+  Button,
   ButtonBase,
+  Collapse,
   IconButton,
   Stack,
   Typography,
+  useMediaQuery,
   useTheme,
 } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-
-import CTLogo from "../../assets/icons/general/Logo-CT.svg";
-import CloseIcon from "@/assets/icons/general/icon-menu-close.svg";
 import {
   ArticleOutlined,
   BedOutlined,
   CalendarMonthOutlined,
   ChevronRight,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
   LocationOnOutlined,
   PersonOutline,
 } from "@mui/icons-material";
@@ -43,6 +45,10 @@ import { getLotNumber } from "@/utils/functions";
 import DetailSection from "@/components/booking/DetailSection";
 import { FormikProps, useFormik } from "formik";
 import * as Yup from "yup";
+
+import CTLogo from "../../assets/icons/general/Logo-CT.svg";
+import CTLogoOnly from "@/assets/icons/general/LogoPrimary.svg";
+import CloseIcon from "@/assets/icons/general/icon-menu-close.svg";
 
 const BookingPage = () => {
   const [stepper, setStepper] = useState<number>(1);
@@ -196,6 +202,9 @@ const BookingPage = () => {
       <BookingStepper
         stepper={stepper}
         handleChangeStepper={handleChangeStepper}
+        bookingSchedule={bookingSchedule}
+        selectedHotel={selectedHotel}
+        roomBookings={roomBookings}
       />
       {stepper === 1 ? (
         <StaySection handleChangeSelectedHotel={handleChangeSelectedHotel} />
@@ -243,6 +252,7 @@ const BookingPage = () => {
 const BookingHeader = () => {
   const theme = useTheme();
   const router = useRouter();
+  const isHandheldDevice = useMediaQuery("(max-width:1050px)");
   return (
     <Box
       display={"flex"}
@@ -252,14 +262,17 @@ const BookingHeader = () => {
       justifyContent={"space-between"}
       alignContent={"center"}
       paddingY={2}
-      paddingX={7}
+      paddingX={isHandheldDevice ? 3 : 7}
       zIndex={2}
       bgcolor={theme.palette.CtColorScheme.black900}
     >
       <Box width={"33%"}></Box>
       <Box display={"flex"} width={"33%"} justifyContent={"center"}>
         <IconButton onClick={() => router.push("/")}>
-          <Image src={CTLogo} alt="CapsuleTransit-Logo" />
+          <Image
+            src={isHandheldDevice ? CTLogoOnly : CTLogo}
+            alt="CapsuleTransit-Logo"
+          />
         </IconButton>
       </Box>
       <Box
@@ -308,8 +321,33 @@ const steps = [
 const BookingStepper = (props: {
   stepper: number;
   handleChangeStepper: (value: number) => void;
+  selectedHotel: IBookingLocation;
+  bookingSchedule: IBookingSchedule;
+  roomBookings: IRoomBooking[];
 }) => {
   const theme = useTheme();
+  const isHandheldDevice = useMediaQuery("(max-width:1050px)");
+  const [handheldExpanded, setHandheldExpanded] = useState<boolean>(false);
+
+  const handleDisabled = (value: string) => {
+    if (value === "Schedule" && props.selectedHotel.hotelName === "") {
+      return true;
+    }
+    if (
+      value === "Room" &&
+      (props.selectedHotel.hotelName === "" || !props.bookingSchedule.date)
+    ) {
+      return true;
+    }
+    if (
+      (value === "Summary" || value === "Details") &&
+      (props.selectedHotel.hotelName === "" ||
+        !props.bookingSchedule.date ||
+        props.roomBookings.length === 0)
+    ) {
+      return true;
+    }
+  };
   return (
     <Stack
       direction={"row"}
@@ -317,36 +355,187 @@ const BookingStepper = (props: {
       justifyContent={"center"}
       alignItems={"center"}
       paddingY={3}
-      spacing={5}
+      spacing={isHandheldDevice ? 0 : 5}
       borderBottom={2}
     >
-      {steps.map((step, index) => (
-        <ButtonBase
-          key={index}
-          onClick={() => props.handleChangeStepper(step.value)}
-        >
-          <Stack
-            direction={"row"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            bgcolor={
-              props.stepper === step.value ? theme.palette.primary.main : ""
-            }
-            padding={1}
-            spacing={1}
+      {!isHandheldDevice ? (
+        steps.map((step, index) => (
+          <ButtonBase
+            key={index}
+            disabled={handleDisabled(step.title)}
+            onClick={() => props.handleChangeStepper(step.value)}
           >
-            {step.icon}
-            <Typography fontWeight={step.value === props.stepper ? 700 : 500}>
-              {step.title}
-            </Typography>
-          </Stack>
-          {index !== steps.length - 1 && (
-            <ChevronRight
-              sx={{ marginLeft: 5, color: theme.palette.CtColorScheme.grey300 }}
-            />
-          )}
-        </ButtonBase>
-      ))}
+            <Stack
+              direction={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              bgcolor={
+                props.stepper === step.value ? theme.palette.primary.main : ""
+              }
+              padding={1}
+              spacing={1}
+            >
+              {step.title === "Stay" ? (
+                <LocationOnOutlined
+                  sx={{
+                    color: handleDisabled(step.title) ? "grey" : "black",
+                  }}
+                />
+              ) : step.title === "Schedule" ? (
+                <CalendarMonthOutlined
+                  sx={{
+                    color: handleDisabled(step.title) ? "grey" : "black",
+                  }}
+                />
+              ) : step.title === "Room" ? (
+                <BedOutlined
+                  sx={{
+                    color: handleDisabled(step.title) ? "grey" : "black",
+                  }}
+                />
+              ) : step.title === "Summary" ? (
+                <ArticleOutlined
+                  sx={{
+                    color: handleDisabled(step.title) ? "grey" : "black",
+                  }}
+                />
+              ) : step.title === "Details" ? (
+                <PersonOutline
+                  sx={{
+                    color: handleDisabled(step.title) ? "grey" : "black",
+                  }}
+                />
+              ) : (
+                ""
+              )}
+              <Typography
+                color={handleDisabled(step.title) ? "grey" : "black"}
+                fontWeight={step.value === props.stepper ? 700 : 500}
+              >
+                {step.title}
+              </Typography>
+            </Stack>
+            {index !== steps.length - 1 && (
+              <ChevronRight
+                sx={{
+                  marginLeft: 5,
+                  color: theme.palette.CtColorScheme.grey300,
+                }}
+              />
+            )}
+          </ButtonBase>
+        ))
+      ) : (
+        <Stack width={"100%"}>
+          <Box
+            display="flex"
+            width={"100%"}
+            paddingX={3}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Stack
+              direction={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              bgcolor={theme.palette.primary.main}
+              padding={1}
+              spacing={1}
+            >
+              {steps[props.stepper - 1].icon}
+              <Typography
+                fontWeight={
+                  steps[props.stepper - 1].value === props.stepper ? 700 : 500
+                }
+              >
+                {steps[props.stepper - 1].title}
+              </Typography>
+            </Stack>
+            <Button
+              onClick={() => setHandheldExpanded((prevValue) => !prevValue)}
+            >
+              <Typography>
+                <b>{props.stepper}</b> of 5
+              </Typography>
+              {handheldExpanded ? (
+                <KeyboardArrowUp color="primary" />
+              ) : (
+                <KeyboardArrowDown color="primary" />
+              )}
+            </Button>
+          </Box>
+          <Collapse in={handheldExpanded}>
+            <Stack width={"100%"} alignItems={"start"} paddingX={4}>
+              {steps
+                .filter((steps) => steps.value !== props.stepper)
+                .map((step, index) => (
+                  <ButtonBase
+                    key={index}
+                    onClick={() => props.handleChangeStepper(step.value)}
+                  >
+                    <Stack
+                      direction={"row"}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                      padding={1}
+                      spacing={1}
+                    >
+                      {step.title === "Stay" ? (
+                        <LocationOnOutlined
+                          sx={{
+                            color: handleDisabled(step.title)
+                              ? "grey"
+                              : "black",
+                          }}
+                        />
+                      ) : step.title === "Schedule" ? (
+                        <CalendarMonthOutlined
+                          sx={{
+                            color: handleDisabled(step.title)
+                              ? "grey"
+                              : "black",
+                          }}
+                        />
+                      ) : step.title === "Room" ? (
+                        <BedOutlined
+                          sx={{
+                            color: handleDisabled(step.title)
+                              ? "grey"
+                              : "black",
+                          }}
+                        />
+                      ) : step.title === "Summary" ? (
+                        <ArticleOutlined
+                          sx={{
+                            color: handleDisabled(step.title)
+                              ? "grey"
+                              : "black",
+                          }}
+                        />
+                      ) : step.title === "Details" ? (
+                        <PersonOutline
+                          sx={{
+                            color: handleDisabled(step.title)
+                              ? "grey"
+                              : "black",
+                          }}
+                        />
+                      ) : (
+                        ""
+                      )}
+                      <Typography
+                        color={handleDisabled(step.title) ? "grey" : "black"}
+                        fontWeight={step.value === props.stepper ? 700 : 500}
+                      >
+                        {step.title}
+                      </Typography>
+                    </Stack>
+                  </ButtonBase>
+                ))}
+            </Stack>
+          </Collapse>
+        </Stack>
+      )}
     </Stack>
   );
 };
