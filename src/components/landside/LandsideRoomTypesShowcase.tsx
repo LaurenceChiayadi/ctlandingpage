dbb2f1e2b1dad3b2ebb664d194fbd507d1cfb55e
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-04-24 11:40:52 +0700

    New Images for Hotel Showcase

--- src/components/landside/LandsideRoomTypesShowcase.tsx
+++ src/components/landside/LandsideRoomTypesShowcase.tsx
@@ -5,34 +5,56 @@ import Image from "next/image";
 import ContentWrapper from "../global/ContentWrapper";
 
 import EyeIcon from "@/assets/icons/general/icon-view-black.svg";
-import Landside1 from "./images/room-types-showcase/img-landside-1@2x.jpg";
-import Landside2 from "./images/room-types-showcase/img-landside-2@2x.jpg";
-import Landside3 from "./images/room-types-showcase/img-landside-3@2x.jpg";
-import Landside4 from "./images/room-types-showcase/img-landside-4@2x.jpg";
-import Landside5 from "./images/room-types-showcase/img-landside-5@2x.jpg";
-import Landside6 from "./images/room-types-showcase/img-landside-6@2x.jpg";
-import Landside7 from "./images/room-types-showcase/img-landside-7@2x.jpg";
-import Landside8 from "./images/room-types-showcase/img-landside-8@2x.jpg";
-import Landside9 from "./images/room-types-showcase/img-landside-9@2x.jpg";
-import Landside10 from "./images/room-types-showcase/img-landside-10@2x.jpg";
-import Landside11 from "./images/room-types-showcase/img-landside-11@2x.jpg";
-import Landside12 from "./images/room-types-showcase/img-landside-12@2x.jpg";
-import Landside13 from "./images/room-types-showcase/img-landside-13@2x.jpg";
-import Landside14 from "./images/room-types-showcase/img-landside-14@2x.jpg";
-import Landside15 from "./images/room-types-showcase/img-landside-15@2x.jpg";
-import Landside16 from "./images/room-types-showcase/img-landside-16@2x.jpg";
-import Landside17 from "./images/room-types-showcase/img-landside-17@2x.jpg";
-import Landside18 from "./images/room-types-showcase/img-landside-18@2x.jpg";
-import Landside19 from "./images/room-types-showcase/img-landside-19@2x.jpg";
-import Landside20 from "./images/room-types-showcase/img-landside-20@2x.jpg";
-import Landside21 from "./images/room-types-showcase/img-landside-21@2x.jpg";
-import Landside22 from "./images/room-types-showcase/img-landside-22@2x.jpg";
-import Landside23 from "./images/room-types-showcase/img-landside-23@2x.jpg";
-import Landside24 from "./images/room-types-showcase/img-landside-24@2x.jpg";
-import Landside25 from "./images/room-types-showcase/img-landside-25@2x.jpg";
-import Landside26 from "./images/room-types-showcase/img-landside-26@2x.jpg";
-import Landside27 from "./images/room-types-showcase/img-landside-27@2x.jpg";
-import Landside28 from "./images/room-types-showcase/img-landside-28@2x.jpg";
+import Landside1 from "./images/room-types-showcase/Capsule-Queen-1.jpg";
+import Landside2 from "./images/room-types-showcase/Capsule-Queen-2.jpg";
+import Landside3 from "./images/room-types-showcase/Capsule-Queen-3.jpg";
+import Landside4 from "./images/room-types-showcase/Capsule-Queen-4.jpg";
+import Landside5 from "./images/room-types-showcase/Capsule-Queen-5.jpg";
+import Landside6 from "./images/room-types-showcase/Capsule-Suite-1.jpg";
+import Landside7 from "./images/room-types-showcase/Capsule-Suite-2.jpg";
+import Landside8 from "./images/room-types-showcase/Capsule-Suite-3.jpg";
+import Landside9 from "./images/room-types-showcase/Capsule-Suite-4.jpg";
+import Landside10 from "./images/room-types-showcase/Capsule-Suite-5.jpg";
+import Landside11 from "./images/room-types-showcase/Capsule-Suite-6.jpg";
+import Landside12 from "./images/room-types-showcase/Capsule-Suite-7.jpg";
+import Landside13 from "./images/room-types-showcase/Capsule-Suite-8.jpg";
+import Landside14 from "./images/room-types-showcase/Capsule-Suite-9.jpg";
+import Landside15 from "./images/room-types-showcase/Female-Pod-1.jpg";
+import Landside16 from "./images/room-types-showcase/Female-Pod-2.jpg";
+import Landside17 from "./images/room-types-showcase/Frontage-1.jpg";
+import Landside18 from "./images/room-types-showcase/Frontage-2.jpg";
+import Landside19 from "./images/room-types-showcase/Frontage-3.jpg";
+import Landside20 from "./images/room-types-showcase/Frontage-4.jpg";
+import Landside21 from "./images/room-types-showcase/Frontage-5.jpg";
+import Landside22 from "./images/room-types-showcase/Frontage-6.jpg";
+import Landside23 from "./images/room-types-showcase/Frontage-7.jpg";
+import Landside24 from "./images/room-types-showcase/Interior-1.jpg";
+import Landside25 from "./images/room-types-showcase/Interior-2.jpg";
+import Landside26 from "./images/room-types-showcase/Interior-3.jpg";
+import Landside27 from "./images/room-types-showcase/Interior-4.jpg";
+import Landside28 from "./images/room-types-showcase/Locker-1.jpg";
+import Landside29 from "./images/room-types-showcase/Locker-2.jpg";
+import Landside30 from "./images/room-types-showcase/Locker-3.jpg";
+import Landside31 from "./images/room-types-showcase/Lounge-1.jpg";
+import Landside32 from "./images/room-types-showcase/Lounge-2.jpg";
+import Landside33 from "./images/room-types-showcase/Lounge-3.jpg";
+import Landside34 from "./images/room-types-showcase/Lounge-4.jpg";
+import Landside35 from "./images/room-types-showcase/Lounge-5.jpg";
+import Landside36 from "./images/room-types-showcase/Lounge-6.jpg";
+import Landside37 from "./images/room-types-showcase/Mixed-Pod-&-Female-Pod-1.jpg";
+import Landside38 from "./images/room-types-showcase/Mixed-Pod-&-Female-Pod-2.jpg";
+import Landside39 from "./images/room-types-showcase/Mixed-Pod-&-Female-Pod-3.jpg";
+import Landside40 from "./images/room-types-showcase/Mixed-Pod-&-Female-Pod-4.jpg";
+import Landside41 from "./images/room-types-showcase/Mixed-Pod-1.jpg";
+import Landside42 from "./images/room-types-showcase/Mixed-Pod-2.jpg";
+import Landside43 from "./images/room-types-showcase/Shared-Bathroom.jpg";
+import Landside44 from "./images/room-types-showcase/Shower-1.jpg";
+import Landside45 from "./images/room-types-showcase/Shower-2.jpg";
+import Landside46 from "./images/room-types-showcase/Shower-3.jpg";
+import Landside47 from "./images/room-types-showcase/Signage-1.jpg";
+import Landside48 from "./images/room-types-showcase/Signage-2.jpg";
+import Landside49 from "./images/room-types-showcase/Signage-3.jpg";
+import Landside50 from "./images/room-types-showcase/Signage-4.jpg";
 import HotelGallery, {
   IHotelGalleryImages,
 } from "../global/hotel/HotelGallery";
@@ -43,119 +65,206 @@ const textContents = ["Have a Glance", "Grid", "List"];
 const LandsideImages: IHotelGalleryImages[] = [
   {
     image: Landside1,
-    category: "Female Single",
+    category: "Queen",
   },
   {
     image: Landside2,
-    category: "Other",
+    category: "Queen",
   },
   {
     image: Landside3,
-    category: "Other",
+    category: "Queen",
   },
-
   {
     image: Landside4,
-    category: "Other",
+    category: "Queen",
   },
   {
     image: Landside5,
-    category: "Other",
+    category: "Queen",
   },
   {
     image: Landside6,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside7,
-    category: "Other",
+    category: "Suite",
   },
 
   {
     image: Landside8,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside9,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside10,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside11,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside12,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside13,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside14,
-    category: "Other",
+    category: "Suite",
   },
   {
     image: Landside15,
-    category: "Other",
+    category: "Female Pod",
   },
   {
     image: Landside16,
-    category: "Other",
+    category: "Female Pod",
   },
   {
     image: Landside17,
-    category: "Other",
+    category: "Frontage",
   },
 
   {
     image: Landside18,
-    category: "Other",
+    category: "Frontage",
   },
   {
     image: Landside19,
-    category: "Other",
+    category: "Frontage",
   },
   {
     image: Landside20,
-    category: "Other",
+    category: "Frontage",
   },
   {
     image: Landside21,
-    category: "Other",
+    category: "Frontage",
   },
 
   {
     image: Landside22,
-    category: "Other",
+    category: "Frontage",
   },
   {
     image: Landside23,
-    category: "Other",
+    category: "Frontage",
   },
   {
     image: Landside24,
-    category: "Other",
+    category: "Interior",
   },
   {
     image: Landside25,
-    category: "Other",
+    category: "Interior",
   },
   {
     image: Landside26,
-    category: "Other",
+    category: "Interior",
   },
   {
     image: Landside27,
-    category: "Other",
+    category: "Interior",
   },
   {
     image: Landside28,
-    category: "Other",
+    category: "Locker",
+  },
+  {
+    image: Landside29,
+    category: "Locker",
+  },
+  {
+    image: Landside30,
+    category: "Locker",
+  },
+  {
+    image: Landside31,
+    category: "Lounge",
+  },
+  {
+    image: Landside32,
+    category: "Lounge",
+  },
+  {
+    image: Landside33,
+    category: "Lounge",
+  },
+  {
+    image: Landside34,
+    category: "Lounge",
+  },
+  {
+    image: Landside35,
+    category: "Lounge",
+  },
+  {
+    image: Landside36,
+    category: "Lounge",
+  },
+  {
+    image: Landside37,
+    category: "Mixed Zone",
+  },
+  {
+    image: Landside38,
+    category: "Mixed Zone",
+  },
+  {
+    image: Landside39,
+    category: "Mixed Zone",
+  },
+  {
+    image: Landside40,
+    category: "Mixed Zone",
+  },
+  {
+    image: Landside41,
+    category: "Mixed Pod",
+  },
+  {
+    image: Landside42,
+    category: "Mixed Pod",
+  },
+  {
+    image: Landside43,
+    category: "Bathroom",
+  },
+  {
+    image: Landside44,
+    category: "Shower",
+  },
+  {
+    image: Landside45,
+    category: "Shower",
+  },
+  {
+    image: Landside46,
+    category: "Shower",
+  },
+  {
+    image: Landside47,
+    category: "Signage",
+  },
+  {
+    image: Landside48,
+    category: "Signage",
+  },
+  {
+    image: Landside49,
+    category: "Signage",
+  },
+  {
+    image: Landside50,
+    category: "Signage",
   },
 ];
 
--- src/components/sleep-lounge/SleepLoungeRoomTypesShowcase.tsx
+++ src/components/sleep-lounge/SleepLoungeRoomTypesShowcase.tsx
@@ -1,27 +1,39 @@
-import SleepLounge1 from "./images/room-types-showcase/img-sleeplounge-1@2x.jpg";
-import SleepLounge2 from "./images/room-types-showcase/img-sleeplounge-2@2x.jpg";
-import SleepLounge3 from "./images/room-types-showcase/img-sleeplounge-3@2x.jpg";
-import SleepLounge4 from "./images/room-types-showcase/img-sleeplounge-4@2x.jpg";
-import SleepLounge5 from "./images/room-types-showcase/img-sleeplounge-5@2x.jpg";
-import SleepLounge6 from "./images/room-types-showcase/img-sleeplounge-6@2x.jpg";
-import SleepLounge7 from "./images/room-types-showcase/img-sleeplounge-7@2x.jpg";
-import SleepLounge8 from "./images/room-types-showcase/img-sleeplounge-8@2x.jpg";
-import SleepLounge9 from "./images/room-types-showcase/img-sleeplounge-9@2x.jpg";
-import SleepLounge10 from "./images/room-types-showcase/img-sleeplounge-10@2x.jpg";
-import SleepLounge11 from "./images/room-types-showcase/img-sleeplounge-11@2x.jpg";
-import SleepLounge12 from "./images/room-types-showcase/img-sleeplounge-12@2x.jpg";
-import SleepLounge13 from "./images/room-types-showcase/img-sleeplounge-13@2x.jpg";
-import SleepLounge14 from "./images/room-types-showcase/img-sleeplounge-14@2x.jpg";
-import SleepLounge15 from "./images/room-types-showcase/img-sleeplounge-15@2x.jpg";
-import SleepLounge16 from "./images/room-types-showcase/img-sleeplounge-16@2x.jpg";
-import SleepLounge17 from "./images/room-types-showcase/img-sleeplounge-17@2x.jpg";
-import SleepLounge18 from "./images/room-types-showcase/img-sleeplounge-18@2x.jpg";
-import SleepLounge19 from "./images/room-types-showcase/img-sleeplounge-19@2x.jpg";
-import SleepLounge20 from "./images/room-types-showcase/img-sleeplounge-20@2x.jpg";
-import SleepLounge21 from "./images/room-types-showcase/img-sleeplounge-21@2x.jpg";
-import SleepLounge22 from "./images/room-types-showcase/img-sleeplounge-22@2x.jpg";
-import SleepLounge23 from "./images/room-types-showcase/img-sleeplounge-23@2x.jpg";
-import SleepLounge24 from "./images/room-types-showcase/img-sleeplounge-24@2x.jpg";
+import SleepLounge1 from "./images/room-types-showcase/Amenities-1.jpg";
+import SleepLounge2 from "./images/room-types-showcase/Amenities-2.jpg";
+import SleepLounge3 from "./images/room-types-showcase/Amenities-3.jpg";
+import SleepLounge4 from "./images/room-types-showcase/Amenities-4.jpg";
+import SleepLounge5 from "./images/room-types-showcase/Amenities-5.jpg";
+import SleepLounge6 from "./images/room-types-showcase/Amenities-6.jpg";
+import SleepLounge7 from "./images/room-types-showcase/Amenities-7.jpg";
+import SleepLounge8 from "./images/room-types-showcase/Coffee-station.jpg";
+import SleepLounge9 from "./images/room-types-showcase/Exterior-1.jpg";
+import SleepLounge10 from "./images/room-types-showcase/Exterior-2.jpg";
+import SleepLounge11 from "./images/room-types-showcase/Exterior-3.jpg";
+import SleepLounge12 from "./images/room-types-showcase/Exterior-4.jpg";
+import SleepLounge13 from "./images/room-types-showcase/Hallway-1.jpg";
+import SleepLounge14 from "./images/room-types-showcase/Hallway-2.jpg";
+import SleepLounge15 from "./images/room-types-showcase/Hallway-3.jpg";
+import SleepLounge16 from "./images/room-types-showcase/Hallway-4.jpg";
+import SleepLounge17 from "./images/room-types-showcase/Interior-1.jpg";
+import SleepLounge18 from "./images/room-types-showcase/Interior-2.jpg";
+import SleepLounge19 from "./images/room-types-showcase/Locker-1.jpg";
+import SleepLounge20 from "./images/room-types-showcase/Locker-2.jpg";
+import SleepLounge21 from "./images/room-types-showcase/Locker-3.jpg";
+import SleepLounge22 from "./images/room-types-showcase/Locker-4.jpg";
+import SleepLounge23 from "./images/room-types-showcase/Locker-5.jpg";
+import SleepLounge24 from "./images/room-types-showcase/Merchandise.jpg";
+import SleepLounge25 from "./images/room-types-showcase/Pod-1.jpg";
+import SleepLounge26 from "./images/room-types-showcase/Pod-2.jpg";
+import SleepLounge27 from "./images/room-types-showcase/Pod-3.jpg";
+import SleepLounge28 from "./images/room-types-showcase/Pod-4.jpg";
+import SleepLounge29 from "./images/room-types-showcase/Pod-5.jpg";
+import SleepLounge30 from "./images/room-types-showcase/Pod-6.jpg";
+import SleepLounge31 from "./images/room-types-showcase/Pod-7.jpg";
+import SleepLounge32 from "./images/room-types-showcase/Reception-1.jpg";
+import SleepLounge33 from "./images/room-types-showcase/Reception-2.jpg";
+import SleepLounge34 from "./images/room-types-showcase/Shoes-Locker.jpg";
+import SleepLounge35 from "./images/room-types-showcase/Signage-1.jpg";
+import SleepLounge36 from "./images/room-types-showcase/Vanity-Room-1.jpg";
 
 import HotelGallery, {
   IHotelGalleryImages,
@@ -30,30 +42,42 @@ import HotelGallery, {
 const textContents = ["Have a Glance", "Grid", "List"];
 
 const AirsideImages: IHotelGalleryImages[] = [
-  { image: SleepLounge1, category: "Other" },
-  { image: SleepLounge2, category: "Other" },
-  { image: SleepLounge3, category: "Other" },
-  { image: SleepLounge4, category: "Other" },
-  { image: SleepLounge5, category: "Other" },
-  { image: SleepLounge6, category: "Other" },
-  { image: SleepLounge7, category: "Other" },
-  { image: SleepLounge8, category: "Other" },
-  { image: SleepLounge9, category: "Other" },
-  { image: SleepLounge10, category: "Other" },
-  { image: SleepLounge11, category: "Other" },
-  { image: SleepLounge12, category: "Other" },
-  { image: SleepLounge13, category: "Other" },
-  { image: SleepLounge14, category: "Other" },
-  { image: SleepLounge15, category: "Other" },
-  { image: SleepLounge16, category: "Other" },
-  { image: SleepLounge17, category: "Other" },
-  { image: SleepLounge18, category: "Other" },
-  { image: SleepLounge19, category: "Other" },
-  { image: SleepLounge20, category: "Other" },
-  { image: SleepLounge21, category: "Other" },
-  { image: SleepLounge22, category: "Other" },
-  { image: SleepLounge23, category: "Other" },
-  { image: SleepLounge24, category: "Other" },
+  { image: SleepLounge1, category: "Amenities" },
+  { image: SleepLounge2, category: "Amenities" },
+  { image: SleepLounge3, category: "Amenities" },
+  { image: SleepLounge4, category: "Amenities" },
+  { image: SleepLounge5, category: "Amenities" },
+  { image: SleepLounge6, category: "Amenities" },
+  { image: SleepLounge7, category: "Amenities" },
+  { image: SleepLounge8, category: "Coffee Station" },
+  { image: SleepLounge9, category: "Exterior" },
+  { image: SleepLounge10, category: "Exterior" },
+  { image: SleepLounge11, category: "Exterior" },
+  { image: SleepLounge12, category: "Exterior" },
+  { image: SleepLounge13, category: "Hallway" },
+  { image: SleepLounge14, category: "Hallway" },
+  { image: SleepLounge15, category: "Hallway" },
+  { image: SleepLounge16, category: "Hallway" },
+  { image: SleepLounge17, category: "Interior" },
+  { image: SleepLounge18, category: "Interior" },
+  { image: SleepLounge19, category: "Locker" },
+  { image: SleepLounge20, category: "Locker" },
+  { image: SleepLounge21, category: "Locker" },
+  { image: SleepLounge22, category: "Locker" },
+  { image: SleepLounge23, category: "Locker" },
+  { image: SleepLounge24, category: "Merchandise" },
+  { image: SleepLounge25, category: "Pod" },
+  { image: SleepLounge26, category: "Pod" },
+  { image: SleepLounge27, category: "Pod" },
+  { image: SleepLounge28, category: "Pod" },
+  { image: SleepLounge29, category: "Pod" },
+  { image: SleepLounge30, category: "Pod" },
+  { image: SleepLounge31, category: "Pod" },
+  { image: SleepLounge32, category: "Reception" },
+  { image: SleepLounge33, category: "Reception" },
+  { image: SleepLounge34, category: "Shoes Locker" },
+  { image: SleepLounge35, category: "Signage" },
+  { image: SleepLounge36, category: "VanityRoom" },
 ];
 
 const SleepLoungeRoomTypesShowcase = () => {
