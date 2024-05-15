bd2f798dc23a904925bc01ed9de3569900f4f505
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2024-05-15 11:25:38 +0700

    Remove Stamp

--- src/components/highlight/HighlightContent.tsx
+++ src/components/highlight/HighlightContent.tsx
@@ -1,4 +1,11 @@
-import { Box, Grid, Stack, Typography, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import ContentWrapper from "../global/ContentWrapper";
 import HeaderBottom from "../global/HeaderBottom";
 import Image from "next/image";
@@ -196,11 +203,6 @@ const LuggageLockerSection = () => {
         <Grid item xs={12} sm={12} md={12} lg={4} xl={4}>
           <Box position={"relative"}>
             <Typography variant="h3">Luggage Storage Locker</Typography>
-            <Image
-              src={IllustrationStandaloneBook}
-              alt="stand-alone"
-              style={{ position: "absolute" }}
-            />
           </Box>
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={4} xl={4}>
@@ -215,6 +217,22 @@ const LuggageLockerSection = () => {
               storage service too! Rent a locker from as low as RM30 and explore
               the airport unencumbered, knowing your luggage is safe with us.
             </Typography>
+            <Stack marginBottom={isHandheldDevice ? 0 : 2}>
+              <Divider
+                sx={{
+                  bgcolor: theme.palette.primary.main,
+                  height: "5px",
+                  marginBottom: 1,
+                }}
+              />
+              <Typography
+                variant="h6"
+                fontWeight={700}
+                sx={{ color: theme.palette.primary.main }}
+              >
+                Available for Standalone Booking!
+              </Typography>
+            </Stack>
           </Stack>
         </Grid>
       </Grid>
@@ -245,16 +263,6 @@ const ShowerServiceSection = () => {
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={4} xl={4}>
           <Box position={"relative"}>
-            <Image
-              src={IllustrationStandaloneBook}
-              alt="stand-alone"
-              style={{
-                position: "absolute",
-                bottom: 150,
-                right: 50,
-                transform: "rotate(20deg)",
-              }}
-            />
             <Typography variant="h3">Refreshing Shower Service</Typography>
           </Box>
         </Grid>
@@ -269,6 +277,22 @@ const ShowerServiceSection = () => {
               at just RM50 per use that comes with a temporary locker to store
               your luggage.
             </Typography>
+            <Stack marginBottom={isHandheldDevice ? 0 : 2}>
+              <Divider
+                sx={{
+                  bgcolor: theme.palette.primary.main,
+                  height: "5px",
+                  marginBottom: 1,
+                }}
+              />
+              <Typography
+                variant="h6"
+                fontWeight={700}
+                sx={{ color: theme.palette.primary.main }}
+              >
+                Available for Standalone Booking!
+              </Typography>
+            </Stack>
           </Stack>
         </Grid>
       </Grid>
